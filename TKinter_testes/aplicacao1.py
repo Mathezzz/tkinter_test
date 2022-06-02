f707b3eae8513557d0f9245aab0e09855f5bca01
Mathezzz <andersonmmelo23@outlook.com>
2022-06-01 23:55:43 -0300

    modificacao janela

--- TKinter_testes/aplicacao1.py
+++ TKinter_testes/aplicacao1.py
@@ -15,11 +15,11 @@ rectangle_1.pack(side='left', ipadx=10, ipady=10, padx=2, fill='x')
 rectangle_2 = tk.Label(main, text='Rectangle 2', bg='gray', fg='black')
 rectangle_2.pack(side='left', ipadx=10, ipady=10, padx=2,fill='x')
 
-rectangle_2 = tk.Label(main, text='Rectangle 2', bg='gray', fg='black')
-rectangle_2.pack(side='left', ipadx=10, ipady=10, padx=2,fill='x')
+rectangle_3 = tk.Label(main, text='Rectangle 3', bg='gray', fg='black')
+rectangle_3.pack(side='left', ipadx=10, ipady=10, padx=2,fill='x')
 
-rectangle_2 = tk.Label(main, text='Rectangle 2', bg='gray', fg='black')
-rectangle_2.pack(side='left', ipadx=10, ipady=10, padx=2,fill='x')
+rectangle_4 = tk.Label(main, text='Rectangle 4', bg='gray', fg='black')
+rectangle_4.pack(side='left', ipadx=10, ipady=10, padx=2,fill='x')
 
 quit_button = ttk.Button(root, text='Sair', command=root.destroy)
 quit_button.pack(side='bottom', fill='both', expand=True)
